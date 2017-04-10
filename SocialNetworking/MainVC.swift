ac23a08bdb99e586f08d7de2c51d33c104687f19
Kiel Delina <KielDelina@Kiels-MacBook-Pro.local>
2017-04-10 02:11:56 -0400

    Setting up App-importing Firebase

---
+++ SocialNetworking/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetworking
+//
+//  Created by Kiel Delina on 2017-04-08.
+//  Copyright © 2017 Dilemma. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
