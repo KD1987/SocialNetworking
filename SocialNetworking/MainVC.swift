1bb8c118eb34d6c312fd586b0ae5e09094a2e95d
Kiel Delina <KielDelina@Kiels-MacBook-Pro.local>
2017-04-11 01:28:00 -0400

    Firebase and Facebook authentication complete

--- SocialNetworking/MainVC.swift
+++ SocialNetworking/MainVC.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
+
 
 class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,35 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func FBBtnTapped(_ sender: Any) {
+        
+        let FBLogin = FBSDKLoginManager()
+        
+        FBLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print ("KD: Unable to authenticate with Facebook")
+            } else if result?.isCancelled == true {
+                print ("KD: User cancelled FB authentication")
+            } else {
+                print ("KD: Successful FB authentication")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    
+    }
+    
+        func firebaseAuth(_ credential: FIRAuthCredential) {
+            
+            FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+                if error != nil {
+                    print ("KD: unable to authenticate with Firebase")
+                } else {
+                    print ("KD: Successful authentication with Firebase")
+                }
+            })
+            
+        }
 
 }
 
