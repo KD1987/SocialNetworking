fbf2290ff6cc510b804279226470952ffc2b2409
Kiel Delina <KielDelina@Kiels-MacBook-Pro.local>
2017-04-16 16:39:16 -0400

    TimelineFeed UI complete

--- SocialNetworking/formatImageView.swift
+++ SocialNetworking/formatImageView.swift
@@ -1,25 +1,28 @@
 //
-//  formatView.swift
+//  formatImageView.swift
 //  SocialNetworking
 //
-//  Created by Kiel Delina on 2017-04-10.
+//  Created by Kiel Delina on 2017-04-16.
 //  Copyright © 2017 Dilemma. All rights reserved.
 //
 
 import UIKit
 
-class formatView: UIView {
-
+class formatImageView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
     
     
+
 }
